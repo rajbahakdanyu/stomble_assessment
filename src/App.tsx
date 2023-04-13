98c7fb08e3778365f0503a45e7f952c416c1039f
Danyu Rajbahak <rajbahakdanyu@gmail.com>
2023-04-13 18:12:36 +1000

    Added prisma

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css"
 
 function App() {
-    return <div>Danyu Rajbahak</div>
+    return (
+        <div>
+            <h1>Stomble Assessment</h1>
+            <input type='text' name='email' id='email' placeholder='Email' />
+            <input
+                type='password'
+                name='password'
+                id='password'
+                placeholder='Password'
+            />
+            <input type='button' value='Login' />
+            <sub>Register an account</sub>
+        </div>
+    )
 }
 
 export default App
