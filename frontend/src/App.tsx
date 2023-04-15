41e1ee4cd9a668c7b0d7dfc296788ad15c9c0744
Danyu Rajbahak <rajbahakdanyu@gmail.com>
2023-04-15 13:42:39 +1000

    Added sorting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,9 @@
+import { useState } from "react"
+
 import "./App.css"
 
 function App() {
-    return (
-        <div>
-            <h1>Stomble Assessment</h1>
-            <input type='text' name='email' id='email' placeholder='Email' />
-            <input
-                type='password'
-                name='password'
-                id='password'
-                placeholder='Password'
-            />
-            <input type='button' value='Login' />
-            <sub>Register an account</sub>
-        </div>
-    )
+    return <div></div>
 }
 
 export default App
